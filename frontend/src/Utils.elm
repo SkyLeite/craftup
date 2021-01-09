765710ee6b9f446fb6779b742c81d6eb21562244
Rodrigo Leite <rodrigo@leite.dev>
2021-01-09 17:05:30 -0300

    Add modal util function

--- frontend/src/Utils.elm
+++ frontend/src/Utils.elm
@@ -1,8 +1,29 @@
-module Utils exposing (onClick)
-import Html
+module Utils exposing (modal, onClick)
+
+import Html exposing (Html, node)
+import Html.Attributes exposing (attribute, class, property)
 import Html.Events
 import Json.Decode
+import Json.Encode exposing (bool)
+
 
 onClick : msg -> Html.Attribute msg
 onClick msg =
     Html.Events.stopPropagationOn "click" (Json.Decode.succeed ( msg, True ))
+
+
+modal : Bool -> List (Html.Attribute msg) -> List (Html msg) -> Html msg
+modal isOpen attributes children =
+    node "dialog"
+        ([ attribute "open"
+            (if isOpen then
+                "true"
+
+             else
+                "false"
+            )
+         , class "mt-16 w-4/5 h-3/6 max-w-2xl"
+         ]
+            ++ attributes
+        )
+        children
