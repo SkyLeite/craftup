2e1a17fa5003fcb465f41caa7ab074d733440712
Sky <sky@leite.dev>
2021-11-07 13:20:54 -0300

    Add icon to Item page

--- frontend/src/DataTypes/Item.elm
+++ frontend/src/DataTypes/Item.elm
@@ -1,20 +1,22 @@
-module DataTypes.Item exposing (Item, itemSearchQuery, itemSelectionSet, itemQuery)
+module DataTypes.Item exposing (Item, itemQuery, itemSearchQuery, itemSelectionSet)
 
 import Api.Object
 import Api.Object.Item
 import Api.Query
 import Graphql.Operation exposing (RootQuery)
-import Graphql.SelectionSet exposing (SelectionSet)
+import Graphql.SelectionSet exposing (SelectionSet, with)
 
 
 type alias Item =
     { name : String
+    , icon : String
     }
 
 
 itemSearchQuery : String -> SelectionSet (List Item) RootQuery
 itemSearchQuery name =
     Api.Query.items { name = name } itemSelectionSet
+
 
 itemQuery : String -> SelectionSet (Maybe Item) RootQuery
 itemQuery name =
@@ -23,4 +25,6 @@ itemQuery name =
 
 itemSelectionSet : SelectionSet Item Api.Object.Item
 itemSelectionSet =
-    Graphql.SelectionSet.map Item Api.Object.Item.name
+    Graphql.SelectionSet.succeed Item
+        |> with Api.Object.Item.name
+        |> with Api.Object.Item.icon
--- frontend/src/Pages/Item.elm
+++ frontend/src/Pages/Item.elm
@@ -1,9 +1,26 @@
 module Pages.Item exposing (view)
+
+import Html exposing (Html, div, img, span, text)
+import Html.Attributes exposing (class, src)
 import Model exposing (Model)
 import Msg exposing (Msg)
-import Html exposing (Html, div, text)
+
+
+iconUrl : String -> String
+iconUrl id =
+    let
+        folder =
+            id |> String.left 2 |> (\x -> "0" ++ x ++ "000")
+
+        file =
+            "0" ++ id
+    in
+    "https://xivapi.com/i/" ++ folder ++ "/" ++ file ++ ".png"
+
 
 view : Model -> String -> Html Msg
 view model name =
-        div [] [ model.foundItem |> Maybe.map .name |> Maybe.withDefault "Not found" |> text ]
-
+    div [ class "flex" ]
+        [ img [ model.foundItem |> Maybe.map .icon |> Maybe.map iconUrl |> Maybe.withDefault "" |> src ] []
+        , span [] [ model.foundItem |> Maybe.map .name |> Maybe.withDefault "Not found" |> text ]
+        ]
