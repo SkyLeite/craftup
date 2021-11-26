2b635c7c05b69f90bf466f802b050cda604f8dc1
Sky <sky@leite.dev>
2021-11-26 19:39:14 -0300

    Add onIncrease and onDecrease to quantity ui element

--- frontend/src/Ui/Quantity.elm
+++ frontend/src/Ui/Quantity.elm
@@ -2,6 +2,7 @@ module Ui.Quantity exposing (init, view, withAttribute, withDisabled)
 
 import Html exposing (Attribute, Html, button, div, input, text)
 import Html.Attributes exposing (attribute, class, pattern, type_, value)
+import Html.Events exposing (onClick)
 import Icons
 
 
@@ -9,12 +10,19 @@ type alias Options msg =
     { attributes : List (Attribute msg)
     , quantity : Int
     , disabled : Bool
+    , onIncrease : msg
+    , onDecrease : msg
     }
 
 
-init : Int -> Options msg
-init quantity =
-    { attributes = [], quantity = quantity, disabled = False }
+init : Int -> ( msg, msg ) -> Options msg
+init quantity ( onIncrease, onDecrease ) =
+    { attributes = []
+    , quantity = quantity
+    , disabled = False
+    , onIncrease = onIncrease
+    , onDecrease = onDecrease
+    }
 
 
 withAttribute : Attribute msg -> Options msg -> Options msg
@@ -37,7 +45,7 @@ view options =
             "flex items-center justify-center w-5 min-h-full h-full rounded bg-green-500 text-center text-white"
     in
     div [ class "flex items-center shadow-sm h-7" ]
-        [ button [ class buttonClasses, class "border-r-0 rounded-r-none" ] [ text "+" ]
+        [ button [ class buttonClasses, class "border-r-0 rounded-r-none", onClick options.onIncrease ] [ text "+" ]
         , input
             ([ type_ "text"
              , attribute "inputmode" "numeric"
@@ -48,5 +56,5 @@ view options =
                 ++ options.attributes
             )
             []
-        , button [ class buttonClasses, class "border-l-0 rounded-l-none" ] [ text "-" ]
+        , button [ class buttonClasses, class "border-l-0 rounded-l-none", onClick options.onDecrease ] [ text "-" ]
         ]
